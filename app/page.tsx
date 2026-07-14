64db9d98712810500353d3ef942acdd113ec0ba2
milles1234 <dy02082003@gmail.com>
2026-07-14 17:40:15 +0530

    Initial

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      Explore Tirupati with Comforts
+      Explore Tirupati with Comforts.
     </div>
   );
 }
