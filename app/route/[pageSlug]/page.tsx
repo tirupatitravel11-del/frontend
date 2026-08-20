bc02faee028a938c1681515ea8f1ce5105688783
milles1234 <dy02082003@gmail.com>
2026-08-20 12:14:06 +0530

    20seater

--- app/route/[pageSlug]/page.tsx
+++ app/route/[pageSlug]/page.tsx
@@ -16,6 +16,8 @@ import EtiosTaxiPage from "@/_components/route/pages/EtiosTaxiPage";
 import TaxiContactNumberPage from "@/_components/route/pages/TaxiContactNumberPage";
 import AmazeTaxiPage from "@/_components/route/pages/AmazeTaxiPage";
 import DistanceAndTravelPage from "@/_components/route/pages/DistanceAndTravelPage";
+import LuxuryTempoTravellerPage from "@/_components/route/pages/LuxuryTempoTravellerPage";
+import Seater20TempoTravellerPage from "@/_components/route/pages/Seater20TempoTravellerPage";
 
 export default async function Page({
   params,
@@ -62,10 +64,15 @@ export default async function Page({
       return <EtiosTaxiPage data={data} />;
     case "taxi-contact-number":
       return <TaxiContactNumberPage data={data} />;
-case "amaze-taxi":
-  return <AmazeTaxiPage data={data} />;
-case "distance-travel-time":
-  return <DistanceAndTravelPage data={data} />;
+    case "amaze-taxi":
+      return <AmazeTaxiPage data={data} />;
+    case "distance-travel-time":
+      return <DistanceAndTravelPage data={data} />;
+
+     case "luxury-tempo-traveller":
+      return <LuxuryTempoTravellerPage data={data} />;  
+      case "20-seater-tempo-traveller":
+      return <Seater20TempoTravellerPage data={data} />; 
     default:
       console.log(
         "Unmatched pageType fell through to 404:",
